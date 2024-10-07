28699cf9ecb54a6f24009d115ca900d7faa24f94
Yves-Dominique <laouagency@gmail.com>
2024-10-07 15:14:31 +0200

    2nd commit

--- terre03.py
+++ terre03.py
@@ -1,6 +1,3 @@
-# ASCII value of n=78 and z=90
-for i in range(ord('n'), ord('z') + 1):
-    # Integer i with chr() will be converted to character
-    # before printing. chr() will take its equivalent
-    # character value
-    print(chr(i), end=" ")
+import sys
+for alphabet in range(ord('n'), ord('z') + 1):
+    print(chr(alphabet), end="")
