a0c69761e6ab7e4dfebf37546c49cbfbd36fd5a4
Yves-Dominique <laouagency@gmail.com>
2024-10-11 23:04:17 +0200

    5th commit

--- terre03.py
+++ terre03.py
@@ -1,3 +1,13 @@
 import sys
-for alphabet in range(ord('n'), ord('z') + 1):
-    print(chr(alphabet), end="")
+
+# Vérifier s'il y a bien un argument fourni
+if len(sys.argv) > 1:
+    start_letter = sys.argv[1]  # Récupérer la lettre donnée en argument
+    if len(start_letter) == 1 and 'a' <= start_letter <= 'z':  # Vérifier que c'est bien une lettre minuscule
+        for letter in range(ord(start_letter), ord('z') + 1):
+            print(chr(letter), end="")
+        print()  # Retour à la ligne après l'alphabet
+    else:
+        print("Veuillez fournir une seule lettre minuscule comme argument.")
+else:
+    print("Veuillez fournir une lettre comme argument.")
