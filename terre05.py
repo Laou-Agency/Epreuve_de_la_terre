0af4134fb506028c5ba96864201d13ca705d6aaf
Yves-Dominique <laouagency@gmail.com>
2024-10-21 15:48:30 +0200

    2nd commit

--- terre05.py
+++ terre05.py
@@ -1,13 +1,44 @@
-def find(n, m):
-    if m == 0 or n < m:
-        print("erreur")
-    else:
-        q = n // m
-        print("résultat:", q)
-        r = n % m
-        print("reste:", r)
+import sys
 
-# drive code
-find(5, 2)
-find(10, 0)
-find(3, 5)
+def get_arguments():
+    # Return the list of arguments passed to the script
+    return sys.argv[1:]
+
+def check_divisor(dvr):
+    # Check if divider equals 0
+    if dvr == 0:
+        print("erreur.")
+        return False
+    return True
+
+def check_dividend(x, y):
+    # Check if dividend is lower than divisor
+    if x < y:
+        print("erreur.")
+        return False
+    return True
+
+def division(dvd, dvr):
+    quotient = dvd // dvr
+    reste = dvd % dvr
+    return quotient, reste
+
+def main():
+    args = get_arguments()
+
+    # Aguments supposed to be even numbers
+    dividend = int(args[0])
+    divisor = int(args[1])
+
+    if not check_divisor(divisor):
+        return
+
+    if not check_dividend(dividend, divisor):
+        return
+
+    quotient, reste = division(dividend, divisor)
+    print(f"résultat: {quotient}")
+    print(f"reste: {reste}")
+
+if __name__ == "__main__":
+    main()
