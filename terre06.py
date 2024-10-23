46643a70d697258838cbd1ea7fd0bc1dbf60a3a0
Yves-Dominique <laouagency@gmail.com>
2024-10-23 10:03:50 +0200

    2nd commit

--- terre06.py
+++ terre06.py
@@ -1,10 +1,24 @@
-# Function to reverse a string
-def reverse (string):
-    string = string[::-1]
-    return string
+import sys
 
-s = "Hello world!"
-st = "lehciM"
+def inverser_chaine(chaine):
+    return chaine[::-1]
 
-print(reverse(s))
-print(reverse(st))
+def main():
+    arguments = sys.argv[1:]
+    
+    if not arguments:
+        print("Erreur : Aucun argument fourni. Veuillez fournir au moins une chaîne à inverser.")
+        return arguments
+
+    resultats = []
+    for arg in arguments:
+        # Inverser chaque chaîne, y compris celles avec des symboles
+        resultats.append(inverser_chaine(arg))
+    
+    # Afficher tous les résultats sur une seule ligne
+    print(" ".join(resultats))
+    
+    return arguments
+
+if __name__ == "__main__":
+    main()
